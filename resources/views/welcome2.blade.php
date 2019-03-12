9e2223b1d4ff191d6e9968315d4d3bd501f0bff5
yohei3rdoct <yohei3rdoct@gmail.com>
2019-03-13 02:39:41 +0900

    bootstrap4.3.1

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -8,6 +8,8 @@
     <meta name="generator" content="Jekyll v3.8.5">
     <title>Album example · Bootstrap</title>
 
+<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+-->
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
