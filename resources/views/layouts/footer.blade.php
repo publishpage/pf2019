a5516a60f468f943990990b12dd5e020f5578be2
yohei3rdoct <yohei3rdoct@gmail.com>
2019-03-31 04:55:19 +0900

    layoutファイル

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,2 @@
+
+<p class='spaceBox'>footer</p>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.layout')
 
 @section('title','第二引数で表記できる')
+
+@section('contents')
+  <div class='spaceBox'>space</div>
+@endsection
